7119201036a7d0ec05f1149a8bea5443669ab8ea
justinlee2012 <justinlee_2012@qq.com>
2016-12-28 17:34:21 +0800

    报销单列表框架已完成2

---
+++ src/modules/FreeBillManager/BillView.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Administrator on 2016/12/26.
+ */
+//ES6
+import React from 'react';
+import {Button, Select, Table} from 'antd'
+
+
+class View extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+        this.closeClick = this.closeClick.bind(this);
+        this.printClick = this.printClick.bind(this);
+
+
+    }
+
+
+    closeClick() {
+        $('#appview').hide();
+    }
+
+    printClick() {
+
+    }
+
+    render() {
+        return (
+
+            <div>
+                <div>
+                    <Button type="ghost" onClick={this.closeClick}>关闭</Button>
+                    <Button type="ghost" onClick={this.printClick}>打印</Button>
+                </div>
+                <div>
+                    我是弹出页面  查看 啊啊啊啊啊啊222
+                </div>
+            </div>
+
+        )
+    }
+}
+
+
+export  default  View;
